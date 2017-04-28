f62269d3f226a3cc183259141fed3ecf84d6c313
Matthew Wroblewski <wroblewski.m@zoho.com>
2017-04-28 10:27:18 -0400

    Added Player functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+/* jshint undef: true, unused: true, asi: true */
+/* globals ctx, Resources, document */
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -6,30 +9,83 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-};
-
+}
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+}
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}
 
 // Now write your own player class
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+  this.x = 200;
+  this.y = 400;
+
+}
 // This class requires an update(), render() and
 // a handleInput() method.
 
+Player.prototype.update = function(){
+
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key){
+    switch(key){
+      case "left":
+          if (this.x > 0){
+              this.x = this.x - 101;
+              this.render();
+          }
+          break;
+      case "right":
+          if (this.x > 0){
+              this.x = this.x + 101;
+              this.render();
+          }
+          break;
+      case "up":
+          if (this.y > 60){
+              this.y = this.y - 83;
+              this.render();
+          }
+           if (this.y < 0) {
+
+              this.render();
+              console.log("we have a winner");
+          }
+          break;
+      case "down":
+          if (this.y < 400){
+              this.y = this.y +83;
+              this.render();
+          }
+
+          break;
+
+    }
+  }
+
+var player = new Player();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+for (var i = 0;i < 5; i++){
+    allEnemies[i] = new Enemy();
+}
 
 
 // This listens for key presses and sends the keys to your
@@ -40,7 +96,7 @@ document.addEventListener('keyup', function(e) {
         38: 'up',
         39: 'right',
         40: 'down'
-    };
+    }
 
     player.handleInput(allowedKeys[e.keyCode]);
-});
+})
